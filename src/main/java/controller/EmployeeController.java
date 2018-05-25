d9c535ba579003c7141bf5a887b11c5c375e1c68
Himedere <nguyenkhacthe94@gmail.com>
2018-05-25 10:14:45 +0700

    Spring Form example

---
+++ src/main/java/controller/EmployeeController.java
@@ -0,0 +1,28 @@
+package controller;
+
+import model.Employee;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class EmployeeController {
+    @RequestMapping(value = "showform", method = RequestMethod.GET)
+    public String showForm (Model model){
+        model.addAttribute("employee", new Employee());
+        return "create";
+    }
+
+    @RequestMapping(value = "addEmployee", method = RequestMethod.POST)
+    public String submit(@ModelAttribute("employee") Employee employee, ModelMap model) {
+        model.addAttribute("name", employee.getName());
+        model.addAttribute("contactNumber", employee.getContactNumber());
+        model.addAttribute("id", employee.getId());
+        return "info";
+    }
+}
